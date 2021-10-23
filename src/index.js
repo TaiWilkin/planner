f9176b575b62c594f73ba69f6226422b8f5450bc
Tai Wilkin-Corraggio <twilkin@azavea.com>
2021-10-23 18:10:52 -0400

    First commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5WOtA5FoGGVVteWWtdp2USKygIiSHyW4",
+  authDomain: "planner-dff98.firebaseapp.com",
+  projectId: "planner-dff98",
+  storageBucket: "planner-dff98.appspot.com",
+  messagingSenderId: "648100518842",
+  appId: "1:648100518842:web:e6b37e2ffe3b0faeb39b83",
+  measurementId: "G-86G8TWLC9Y"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
